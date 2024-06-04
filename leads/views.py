c78fa73f9048c5de9cc536a43e38a5ca1a44e021
dhir <dhirajdev.stuff@gmail.com>
2024-06-04 19:23:04 +0530

    added models

--- leads/views.py
+++ leads/views.py
@@ -1,3 +1,31 @@
 from django.shortcuts import render
+from .models import Lead
 
-# Create your views here.
+from django.http import HttpResponse
+
+from .forms import LeadForm
+
+
+def lead_list(request):
+    leads = Lead.objects.all()
+
+    context = {
+        'leads':leads
+    }
+    return render(request, "leads/lead-list.html", context)
+
+
+def lead_detail(request, pk):
+    lead = Lead.objects.get(id=pk)
+    context = {
+        "lead":lead
+    }    
+    return render(request, "leads/lead-detail.html", context)
+
+
+def lead_create(request):
+    print(request.POST)
+    context = {
+        "form": LeadForm()
+    }
+    return render(request, "leads/lead-create.html", context) 
